db18ac4bab94afa7b21a92eb4472fb5eb11a86c1
Duncan Finney <duncan.finney@gmail.com>
2015-12-05 14:56:29 -0500

    initial commit and problems 1 and 2

---
+++ src/Day2.scala
@@ -0,0 +1,37 @@
+object Day2 extends App {
+
+  case class Box(l: Int, w: Int, h: Int) {
+
+    val sideAreas = List(l * w, w * h, h * l)
+    val sideDiameters = List(2 * l + 2 * w, 2 * w + 2 * h, 2 * h + 2 * l)
+
+    def neededWrappingPaper = 2 * sideAreas.sum + sideAreas.min
+
+    def neededRibbon = sideDiameters.min + (w * l * h)
+  }
+
+  assert(Box(2, 3, 4).neededWrappingPaper == 58)
+  assert(Box(2, 3, 4).neededRibbon == 34)
+
+
+  val input = scala.io.Source.fromFile("src/input/Day2.txt", "utf8").mkString
+
+  val inputAsBoxes = input
+    .split("\n")
+    .map(line => {
+      val Array(l, w, h) = line.split('x')
+      Box(l.toInt, w.toInt, h.toInt)
+    })
+
+  val partOne = inputAsBoxes
+    .map(_.neededWrappingPaper)
+    .sum
+
+  val partTwo = inputAsBoxes
+    .map(_.neededRibbon)
+    .sum
+
+  println("part1", partOne)
+  println("part2", partTwo)
+
+}
