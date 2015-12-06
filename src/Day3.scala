3f6e79a2914d0a02d232add91ee8df819ea33118
Duncan Finney <duncan.finney@gmail.com>
2015-12-06 09:10:54 -0500

    Day3 part2

--- src/Day3.scala
+++ src/Day3.scala
@@ -5,14 +5,14 @@ object Day3 extends App {
   type Direction = Char
 
   case class Point(x: Int, y: Int) {
-    def +(p :(Int,Int)) = Point(x + p._1, y + p._2)
+    def +(p: (Int, Int)) = Point(x + p._1, y + p._2)
   }
 
   def nextLocation(loc: Point, dir: Direction) = dir match {
-    case '<' => loc + (-1, 0)
-    case '>' => loc + (1, 0)
-    case '^' => loc + (0, 1)
-    case 'v' => loc + (0, -1)
+    case '<' => loc +(-1, 0)
+    case '>' => loc +(1, 0)
+    case '^' => loc +(0, 1)
+    case 'v' => loc +(0, -1)
     case _ => throw new Exception("Not found")
   }
 
@@ -30,14 +30,36 @@ object Day3 extends App {
   }
 
   def partOne = {
-    val initialState = PartOneState(Map(), Point(0,0))
+    val initialState = PartOneState(Map(), Point(0, 0))
     val finalState = input.foldLeft(initialState)(doPartOneMove)
     finalState.visited.size
   }
 
   //part two
-  //TODO: Finish me
+  object SantaType extends Enumeration {
+    type SantaType = Value
+    val Santa, RobotSanta = Value
+  }
 
+  import SantaType._
+  case class PartTwoState(visited: Set[Point], santaLocation: Point, robotLocation: Point, nextMove: SantaType) {
+
+    def doMove(direction: Direction): PartTwoState = if (nextMove == SantaType.Santa) {
+      val newSantaLocation = nextLocation(santaLocation, direction)
+      PartTwoState(visited + newSantaLocation, newSantaLocation, robotLocation, SantaType.RobotSanta)
+    } else {
+      val newRobotLocation = nextLocation(robotLocation, direction)
+      PartTwoState(visited + newRobotLocation, santaLocation, newRobotLocation, SantaType.Santa)
+    }
+
+  }
+
+  def partTwo = {
+    val initialState = PartTwoState(Set(), Point(0,0), Point(0,0), SantaType.Santa)
+    val finalState = input.foldLeft(initialState)((state, move) => state.doMove(move))
+    finalState.visited.size
+  }
 
   println("part1", partOne)
+  println("part2", partTwo)
 }
