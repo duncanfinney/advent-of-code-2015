7cd7e3782177115fc31c9bc0c0d208de9a4dc290
Duncan Finney <duncan.finney@gmail.com>
2015-12-25 05:38:14 -0500

    Finally got Day19

--- build.sbt
+++ build.sbt
@@ -6,7 +6,8 @@ mainClass in (Compile, run) := Some("main.scala.Day12")
 libraryDependencies ++= Seq(
   "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test",
   "com.typesafe.play" %% "play-json" % "2.3.4",
-  "com.lihaoyi" %% "fastparse" % "0.3.4"
+  "com.lihaoyi" %% "fastparse" % "0.3.4",
+  "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.2"
 )
 
 resolvers ++= Seq("Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/")
--- src/main/scala/Day19.scala
+++ src/main/scala/Day19.scala
@@ -15,7 +15,7 @@ object Day19 extends App {
 
   }
 
-  //part1
+  //  part1
   val part1 = input
     .flatMap(getCombinationsForLine(initialMolecule, _))
     .distinct
@@ -25,30 +25,48 @@ object Day19 extends App {
 
   //part2
   case class Replacement(from: String, to: String) {
-    def getNeighbors(str: String): List[String] = {
-      val replaceRegex = from.r
-      (for (m <- replaceRegex.findAllMatchIn(str)) yield str.substring(0, m.start) ++ to ++ str.substring(m.end, str.length)).toList
-    }
-  }
 
-  val backgwardsReplacement = input.map(line => {
-    val parseRegex(from, to) = line
-    Replacement(to, from) //backwards replacement for part two
-  })
+    def inverse = Replacement(to, from)
 
-  def getReverseNeighbors(str: String): Stream[(String, Int)] = {
-    def neighborsOf(pair: (String, Int)) = {
-        backgwardsReplacement
-          .flatMap(_.getNeighbors(pair._1))
-          .sortBy(_.length)
-          .map(s => (s, pair._2 + 1))
-          .toStream
+    def applyTo(s: String) = {
+      (for (m <- from.r.findAllMatchIn(s))
+        yield s.substring(0, m.start) ++ to ++ s.substring(m.end, s.length)).toList
     }
 
-    lazy val neighbors: Stream[(String, Int)] = (str, 0) #:: neighbors.flatMap(neighborsOf)
-    neighbors
   }
 
-    getReverseNeighbors(initialMolecule).find(_._1 == "e")
+  val replacements = input.map(_.split("=>")).map { case Array(lhs, rhs) => Replacement(lhs.trim, rhs.trim) }.toList
+  val inverseReplacements = replacements.map(_.inverse)
+
+  case class Node(str: String, distance: Int = 0) {
+    lazy val neighbors: Set[Node] =
+      inverseReplacements
+        .flatMap(_.applyTo(str))
+        .sortBy(_.length)
+        .map(Node(_, distance + 1))
+        .toSet
+  }
+
+
+  def findNode(str: String, startNode: Node): Option[Node] = {
+
+    @tailrec
+    def findNodeInner(currNode: Node, visitedNodes: Set[Node]): Option[Node] = {
+      val allNeighbors = visitedNodes.flatMap(_.neighbors).filter(!visitedNodes.contains(_)).toList
+      if (currNode.str == str) Some(currNode)
+      else if (allNeighbors.isEmpty) None
+      else {
+        allNeighbors.sortBy(_.str.length).headOption match {
+          case Some(n) => findNodeInner(n, visitedNodes + currNode)
+          case None => None
+        }
+      }
+    }
+
+    findNodeInner(startNode, Set(startNode))
+  }
+
+  val startNode = Node(initialMolecule)
+  println("part2", findNode("e", startNode))
 
 }
